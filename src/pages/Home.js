cd32ce2cdd232cda4ce39675dc452c930ebf9b49
Kamil Lazarski <lazarski.k.s@gmail.com>
2024-02-20 21:53:42 +0100

    init4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
 import supabase from "../config/supabaseClient";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select("*");
+      if (error) {
+        setFetchError(error);
+      } else {
+        setSmoothies(data);
+      }
+    };
+    fetchSmoothies();
+  };
+
   return (
     <div className="page home">
       <h2>Home</h2>
